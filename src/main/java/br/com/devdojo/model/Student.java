f4f0ed6774b65fcd53fc578a869d2ad101522c51
julianwelter <julianwelterdejesus@gmail.com>
2020-09-24 18:47:50 -0300

    Ligando ao banco

--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,35 +1,19 @@
 package br.com.devdojo.model;
 
+import javax.persistence.Entity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import static java.util.Arrays.asList;
-
-public class Student {
+@Entity
+public class Student  extends AbstractEntity{
     private String name;
-    private int id;
-    public Student(String name){
-        this.name=name;
-    }
-    public static List<Student> studentList;
 
-    static{
-        studentRepository();
-    }
-
-    public Student(String name, int id) {
-        this.name=name;
-        this.id = id;
-    }
-    public Student(int id){
-        this.id=id;
+    public Student(String name) {
+        this.name = name;
     }
     public Student(){
-    }
-
-    private static void studentRepository(){
-        studentList=new ArrayList<>(asList(new Student("Deku",1), new Student("Todoroki",2)));
     }
 
     public String getName() {
@@ -38,26 +22,5 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
     }
 }
